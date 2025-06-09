762600a6e5190333158f4d6cfbfca5819f29e8b9
MokkapalliPrudviRaj <mokkapalliprudviraj1132001@gmail.com>
2025-06-09 21:56:21 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule], // ✅ Required for ngClass
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tooljetassignment';
+  
+  @ViewChild('carouselTrack', { static: true }) track!: ElementRef;
+
+  scrollAmount = 0;
+  itemWidth = 320; // Adjust based on your actual card+gap width
+
+  scrollForward(): void {
+    this.scrollAmount += this.itemWidth;
+    this.track.nativeElement.scrollTo({
+      left: this.scrollAmount,
+      behavior: 'smooth'
+    });
+  }
+
+  scrollBackward(): void {
+    this.scrollAmount -= this.itemWidth;
+    if (this.scrollAmount < 0) {
+      this.scrollAmount = 0;
+    }
+    this.track.nativeElement.scrollTo({
+      left: this.scrollAmount,
+      behavior: 'smooth'
+    });
+  }
+
 }
